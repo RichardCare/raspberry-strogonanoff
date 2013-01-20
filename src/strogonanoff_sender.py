40b665d083a9e549326d066b2c0bcda15463ad4d
Mark Himsley <mark@mdsh.com>
2013-01-20 18:06:26 +0000

    Previous implementation was very opaque and very odd.
    This implementation follows the detail described in from the following
    web site, and is hopefully much less opaque.
    
    http://elektronikforumet.com/wiki/index.php/RF_Protokoll_-_Nexa/Proove_%28%C3%A4ldre,_ej_sj%C3%A4lvl%C3%A4rande%29

--- src/strogonanoff_sender.py
+++ src/strogonanoff_sender.py
@@ -1,72 +1,52 @@
+#!/usr/bin/env python
+
 from time import time
 from strogonanoff_common import *
-preamble = [0] * 26
-sync = [1]
-postamble = [0] * 2
 
+def print_bitstream(state_list):
+    val = ""
+    for state in state_list: val += str(state)
+    print "data:", val
 
-# converts the lowest bit_count bits to a list of ints
-def int_to_bit_list(i, bit_count):
-    result = []
-    shifted = i
-    for i in range(0, bit_count):
-        result.append(shifted & 0x01)
-        shifted = shifted >> 1
-    return result
-
-# encodes 0 as a 1 count state change, 1 as a 3 count state change, starting
-# with a change to low
-def encode_as_state_list(bit_list):
-    result = []
-    state = 0
-    for bit in bit_list:
-        result.extend([state] if bit == 0 else [state, state, state])
-        state = 1 - state
-    return result
-
-def encode_packet(bit_list):
-    return preamble + sync + encode_as_state_list(bit_list) + postamble
-
-def command_as_bit_list(channel, button, on):
-    return int_to_bit_list(
-        channel_codes[channel - 1][button - 1], 32) + \
-        int_to_bit_list(on_code if on else off_code, 16)
+def print_send_info(pin_number, pulse_width, state_list):
+    print "send to pin:", pin_number.gpio_number, \
+          "pulse width:", pulse_width/1e-6, "us"
+    print_bitstream(state_list)
 
 def busy_wait_until(end_time):
-    while (time() <= end_time): pass
+    while time() <= end_time: pass
 
-def send(pin_number, state_list, pulse_width):
-     end_time = time()
-     for state in state_list:
-         end_time = end_time + pulse_width
-         pin.set_value(state)
-         busy_wait_until(end_time)
+def send_bitstream(pin_number, state_list, verbose, pulse_width):
+    if verbose: print_send_info(pin_number, pulse_width, state_list)
+    end_time = time()
+    for state in state_list:
+        end_time = end_time + pulse_width
+        pin.set_value(state)
+        busy_wait_until(end_time)
 
-# still not fast enough
-def quick2wire_send(pin_number, state_list, pulse_width):
-    with exported(Pin(pin_number, Pin.Out)) as pin:
-        end_time = time()
-        for state in state_list:
-            end_time = end_time + pulse_width
-            pin.value = state
-            busy_wait_until(end_time)
+def encode_bitstream(channel, button, on):
+    result = []
+    for i in range(6):
+        result = result + BIT_CODE[channel-1] + BIT_CODE[button-1] + UNKNOWN_CODE
+        result = result + ON_CODE if on == 1 else result + OFF_CODE
+        result = result + SYNC
+    return result
 
-
-def send_command(pin, channel, button, on, pulse_width = default_pulse_width):
-    send(pin, encode_packet(command_as_bit_list(channel, button, on)), pulse_width)
+def send_command(pin, channel, button, on, verbose, pulse_width=DEFAULT_PULSE_WIDTH):
+    send_bitstream(pin, encode_bitstream(channel, button, on), verbose, pulse_width)
 
 if __name__ == "__main__":
 
     from WiringPin import WiringPin
     from optparse import OptionParser
 
-
     parser = OptionParser()
-    parser.add_option("-b", "--button", type = "int", default = 1)
-    parser.add_option("-c", "--channel", type = "int", default = 1)
-    parser.add_option("-g", "--gpio", type = "int", default = 0)
+    parser.add_option("-b", "--button", type="int", default=1)
+    parser.add_option("-c", "--channel", type="int", default=1)
+    parser.add_option("-g", "--gpio", type="int", default=0)
+    parser.add_option("-v", "--verbose", action="store_true", default = False)
     (options, args) = parser.parse_args()
     on = True if len(args) == 0 or args[0] != "off" else False
     pin = WiringPin(options.gpio).export()
-    for i in range(1, 6):
-        send_command(pin, options.channel, options.button, on)
+    send_command(pin, options.channel, options.button, on, options.verbose)
+
